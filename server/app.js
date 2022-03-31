cb37d2842efc31d3983232adb207a50c433b2c49
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2022-03-31 07:44:31 -0300

    Installation of passport, bcrypt and sqlite3 Uninstalled Auth0 to try out passportjs, sqlite is going to be a database for tests

--- server/app.js
+++ server/app.js
@@ -2,6 +2,44 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+// Option 2: Passing parameters separately (sqlite)
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '/home/tommy/Documents/Projects/petProjects/assistoDB/Assisto.db',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(() => console.error('Unable to connect to the database:', error));
+
+const User = sequelize.define(
+  'User',
+  {
+    // Model attributes are defined here
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User);
+
+User.sync();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
